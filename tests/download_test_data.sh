7f84b1ae500c08d63729b9dd144e437f53facd6d
Bruce Edelman <bruce.edelman@ligo.org>
2023-06-01 11:53:27 -0700

    try again to fix testing. install numpyro/jax with pip instead of conda

--- tests/download_test_data.sh
+++ tests/download_test_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget -O tests/data/downsampled.tar.gz "https://dcc.ligo.org/public/0182/T2200137/001/O3bPE_downsampled.tar.gz"
-tar -xvzf tests/data/downsampled.tar.gz 
-wget -O tests/data/injections.h5 "https://zenodo.org/record/5546676/files/endo3_bbhpop-LIGO-T2100113-v12.hdf5"
+cd tests/data
+wget -O downsampled.tar.gz "https://dcc.ligo.org/public/0182/T2200137/001/O3bPE_downsampled.tar.gz"
+tar -xvzf downsampled.tar.gz
+wget -O injections.h5 "https://zenodo.org/record/5546676/files/endo3_bbhpop-LIGO-T2100113-v12.hdf5"
+cd ../../
