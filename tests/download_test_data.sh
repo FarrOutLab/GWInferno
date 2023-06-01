78f6a2931004da2719424d93a181d528ca2b5dab
Bruce Edelman <bruce.edelman@ligo.org>
2023-06-01 11:29:52 -0700

    switch up testing

--- tests/download_test_data.sh
+++ tests/download_test_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd tests/data
-wget -O downsampled.tar.gz "https://dcc.ligo.org/public/0182/T2200137/001/O3bPE_downsampled.tar.gz"
-tar -xvzf downsampled.tar.gz
-wget -O injections.h5 "https://zenodo.org/record/5546676/files/endo3_bbhpop-LIGO-T2100113-v12.hdf5"
-cd ../../
+wget -O tests/data/downsampled.tar.gz "https://dcc.ligo.org/public/0182/T2200137/001/O3bPE_downsampled.tar.gz"
+tar -xvzf tests/data/downsampled.tar.gz tests/data/
+wget -O tests/data/injections.h5 "https://zenodo.org/record/5546676/files/endo3_bbhpop-LIGO-T2100113-v12.hdf5"
