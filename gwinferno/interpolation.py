1388d8382cf3707afb1d65df8ef8797a0efe9ad6
Jaxen  Godfrey <80279129+jaxengodfrey@users.noreply.github.com>
2023-08-07 14:02:07 -0700

    small bug fix

--- gwinferno/interpolation.py
+++ gwinferno/interpolation.py
@@ -99,7 +99,7 @@ class BasisSpline(object):
             dx = interior_knots[1] - interior_knots[0]
             knots = jnp.linspace(xrange[0] - dx * (k - 1), xrange[1] + dx * (k - 1), len(interior_knots) + (k - 1) * 2)
         self.knots = knots
-        self.interior_knots = (xrange[1] - xrange[0]) * interior_knots
+        self.interior_knots = interior_knots
         assert len(self.knots) == self.N + self.order
 
         self.normalize = normalize
