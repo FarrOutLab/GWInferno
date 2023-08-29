0f322038215e1b74a316ab1b3d723cdbfb85f5ee
Jaxen Godfrey <jaxeng@uoregon.edu>
2023-08-29 11:57:55 -0700

    no change I think

--- gwinferno/utils.py
+++ gwinferno/utils.py
@@ -6,6 +6,16 @@ import importlib
 import importlib.resources
 from functools import lru_cache
 from arviz.data.base import _extend_xr_method
+from numpyro.infer import MCMC, NUTS, init_to_sample, DiscreteHMCGibbs
+from jax import random
+from jax.tree_util import tree_flatten, tree_map
+import deepdish as dd
+from numpyro.diagnostics import split_gelman_rubin
+from jax import device_get
+import arviz as az
+import numpy as np
+from numpyro.diagnostics import effective_sample_size
+from jax.lax import while_loop
 
 """
 This script is adapted from arviz.inference_data https://python.arviz.org/en/stable/_modules/arviz/data/inference_data.html and is a more generalized form of arviz's InferenceData object that can accept any type of of group. It is a data structure for using netcdf groups with xarray.
@@ -372,3 +382,105 @@ class DataSet(Mapping[str, xr.Dataset]):
     max = _extend_xr_method(xr.Dataset.max, see_also=["min", "sum"])  # type: ignore[attr-defined]
     cumsum = _extend_xr_method(xr.Dataset.cumsum, see_also="sum")  # type: ignore[attr-defined]
     sum = _extend_xr_method(xr.Dataset.sum, see_also="cumsum")  # type: ignore[attr-defined]
+
+
+
+
+
+def compute_rhats(data, threshold = 1.001, num_chains = 1):
+    if num_chains == 1 : data = tree_map(lambda x: x[None, ...], data) 
+    rhats = []
+    for name, value in data.items():
+            value = device_get(value)
+            rhat = split_gelman_rubin(value)
+            if isinstance(rhat, np.ndarray):
+                rhats.extend(rhat)
+            else:
+                rhats.append(rhat)
+    sel = np.array(rhats) < threshold
+    tot = len(sel)
+    keep_sampling = True
+    percent_not_converged = (sum(~sel)/tot)*100
+    if (sum(~sel)/tot)*100 < 0.1:
+        keep_sampling = False
+        print(f'it is recommended to stop sampling')
+    else:
+        print(f'it is recommended to continue sampling as {percent_not_converged:.0f}% of parameters have rhat above {threshold}')
+
+    return keep_sampling
+
+
+def compute_effective_sample_size(data, threshold = 1000, num_chains = 1):
+    if num_chains == 1 : data = tree_map(lambda x: x[None, ...], data)
+    for name, value in data.items():
+        n_eff = effective_sample_size(value)
+    keep_sampling = True
+    if n_eff > threshold:
+        keep_sampling = False
+        print(f'it is recommended to stop sampling')
+    else:
+        print(f'it is recommended to continue sampling as {n_eff:.0f} is below the threshold value of {threshold}')
+    return keep_sampling
+
+
+def checkpoint(kernel, rng_key,  file_name, threshold, statistic = 'rhat', file_path = '', num_warmup = 50000, max_samples = 100000, num_chains = 1, thinning = 1, model_kwargs = {}, mcmc_kwargs = {}):
+
+    if statistic == 'rhat':
+        statfunc = compute_rhats
+
+    elif statistic == 'n_eff':
+        statfunc = compute_effective_sample_size
+    else:
+        raise ValueError('only effective sample size (n_eff) and gelman rubin diagnostic (rhat) are supported')
+
+    num_samples = int(max_samples / 10)
+    MCMC_RNG, PRIOR_RNG, _RNG = random.split(rng_key, num=3)
+
+    mcmc = MCMC(
+            kernel,
+            thinning=thinning,
+            num_warmup=num_warmup,
+            num_samples=num_samples,
+            num_chains=num_chains,
+            **mcmc_kwargs
+        )
+
+    mcmc.run(
+                MCMC_RNG,
+                **model_kwargs
+            )
+
+    count = 1
+    first_10percent_of_samples = mcmc.get_samples()
+    first_10percent_idata = az.from_numpyro(mcmc)
+    DataSet(posterior = first_10percent_idata.posterior).to_netcdf(file_path + file_name + f'_{num_samples}x{count}_dataset.h5')
+    print(f'checkpoint file {count} saved')
+    keep_sampling = statfunc(first_10percent_of_samples, threshold = threshold, num_chains = num_chains)
+
+    while keep_sampling:
+        mcmc.post_warmup_state = mcmc.last_state
+        mcmc.run(mcmc.post_warmup_state.rng_key, **model_kwargs)
+        next_10percent_of_samples = mcmc.get_samples()
+        next_10percent_idata = az.from_numpyro(mcmc)
+        count += 1
+        DataSet(posterior = next_10percent_idata.posterior).to_netcdf(file_path + file_name + f'_{num_samples}x{count}_dataset.h5', )
+        print(f'checkpoint file {count} saved')
+        if count == 10:
+            keep_sampling = False
+            print('number of samples has exceeded max samples, sampling stopped')
+        else:
+            keep_sampling = statfunc(next_10percent_of_samples, threshold = threshold, num_chains = num_chains)
+
+    if count > 1:
+        dataset = DataSet.from_netcdf(file_path + file_name + f'_{num_samples}x1_dataset.h5')
+        dataset = dataset.posterior
+        for i in np.arange(2,count+1):
+            dat = DataSet.from_netcdf(file_path + file_name + f'_{num_samples}x{i}_dataset.h5')
+            dat.posterior['draw'] = dat.posterior['draw'] + num_samples*(i-1)
+            dataset = xr.merge([dataset, dat.posterior])
+        DataSet(posterior = dataset).to_netcdf(file_path + file_name + f'_{num_samples*count}s_merged{count}_dataset.h5')
+
+        return dataset
+
+    else:
+        return first_10percent_idata.posterior
