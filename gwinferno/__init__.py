2ba7ca3a278c6bc6e0fffd31cba58762654be699
Ben Farr <bfarr@uoregon.edu>
2023-09-19 12:59:28 -0700

    fix with pre-commit

--- gwinferno/__init__.py
+++ gwinferno/__init__.py
@@ -2,10 +2,10 @@ from . import cosmology
 from . import distributions
 from . import interpolation
 from . import numpyro_distributions
-from . import utils
 from . import parameter_estimation
 from . import pipeline
 from . import postprocess
 from . import preprocess
+from . import utils
 
 __version__ = "0.0.2"
--- gwinferno/utils.py
+++ gwinferno/utils.py
@@ -1,27 +1,36 @@
-import xarray as xr
-from html import escape
-import uuid
-from arviz.utils import HtmlTemplate
 import importlib
 import importlib.resources
+import re
+import uuid
 from functools import lru_cache
-from arviz.data.base import _extend_xr_method
-from numpyro.infer import MCMC, NUTS, init_to_sample, DiscreteHMCGibbs
-from jax import random
-from jax.tree_util import tree_flatten, tree_map
-import deepdish as dd
-from numpyro.diagnostics import split_gelman_rubin
-from jax import device_get
+from html import escape
+from typing import Any
+from typing import Iterator
+from typing import List
+from typing import Mapping
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
 import arviz as az
 import numpy as np
+import xarray as xr
+from arviz.data.base import _extend_xr_method
+from jax import device_get
+from jax import random
+from jax.tree_util import tree_map
 from numpyro.diagnostics import effective_sample_size
-from jax.lax import while_loop
+from numpyro.diagnostics import split_gelman_rubin
+from numpyro.infer import MCMC
 
 """
-This script is adapted from arviz.inference_data https://python.arviz.org/en/stable/_modules/arviz/data/inference_data.html and is a more generalized form of arviz's InferenceData object that can accept any type of of group. It is a data structure for using netcdf groups with xarray.
+This script is adapted from arviz.inference_data https://python.arviz.org/en/stable/_modules/arviz/data/inference_data.html
+and is a more generalized form of arviz's InferenceData object that can accept any type of of group. It is a data structure
+for using netcdf groups with xarray.
 """
 
 STATIC_FILES = ("static/html/icons-svg-inline.html", "static/css/style.css")
+
 
 @lru_cache(None)
 def _load_static_files():
@@ -29,9 +38,8 @@ def _load_static_files():
 
     Clone from xarray.core.formatted_html_template.
     """
-    return [
-        importlib.resources.files("arviz").joinpath(fname).read_text() for fname in STATIC_FILES
-    ]
+    return [importlib.resources.files("arviz").joinpath(fname).read_text() for fname in STATIC_FILES]
+
 
 class HtmlTemplate:
     """Contain html templates for DataSet repr."""
@@ -63,18 +71,6 @@ class HtmlTemplate:
     css_template = f"<style> {css_style}{specific_style} </style>"
 
 
-from typing import (
-    Any,
-    List,
-    Iterator,
-    Mapping,
-    Tuple,
-    Union,
-    Optional,
-    overload,
-)
-
-
 def _compressible_dtype(dtype):
     """Check basic dtypes for automatic compression."""
     if dtype.kind == "V":
@@ -84,12 +80,13 @@ def _compressible_dtype(dtype):
 
 class DataSet(Mapping[str, xr.Dataset]):
     """Adapted from Arviz InferenceData object https://python.arviz.org/en/stable/_modules/arviz/data/inference_data.html#InferenceData"""
+
     def __init__(
         self,
         attrs: Union[None, Mapping[Any, Any]] = None,
         **kwargs: Union[xr.Dataset, List[xr.Dataset], Tuple[xr.Dataset, xr.Dataset]],
     ) -> None:
-        
+
         self._groups: List[str] = []
         self._attrs: Union[None, dict] = dict(attrs) if attrs is not None else None
         key_list = [key for key in kwargs]
@@ -105,7 +102,7 @@ class DataSet(Mapping[str, xr.Dataset]):
     def __len__(self) -> int:
         """Return the number of groups in this DataSet object."""
         return len(self._groups)
-    
+
     def __iter__(self) -> Iterator[str]:
         """Iterate over groups in DataSet object."""
         for group in self._groups:
@@ -116,16 +113,14 @@ class DataSet(Mapping[str, xr.Dataset]):
         if key not in self._groups:
             raise KeyError(key)
         return getattr(self, key)
-    
+
     def groups(self) -> List[str]:
-            """Return all groups present in DataSet object."""
-            return self._groups_all
-    
+        """Return all groups present in DataSet object."""
+        return self._groups_all
+
     def __repr__(self) -> str:
         """Make string representation of DataSet object."""
-        msg = "Inference data with groups:\n\t> {options}".format(
-            options="\n\t> ".join(self._groups)
-        )
+        msg = "Inference data with groups:\n\t> {options}".format(options="\n\t> ".join(self._groups))
         return msg
 
     def _repr_html_(self) -> str:
@@ -142,19 +137,15 @@ class DataSet(Mapping[str, xr.Dataset]):
                         HtmlTemplate.element_template.format(
                             group_id=group + str(uuid.uuid4()),
                             group=group,
-                            xr_data=getattr(  # pylint: disable=protected-access
-                                self, group
-                            )._repr_html_(),
+                            xr_data=getattr(self, group)._repr_html_(),  # pylint: disable=protected-access
                         )
                         for group in self._groups_all
                     ]
                 )
-                formatted_html_template = (  # pylint: disable=possibly-unused-variable
-                    HtmlTemplate.html_template.format(elements)
-                )
+                formatted_html_template = HtmlTemplate.html_template.format(elements)  # pylint: disable=possibly-unused-variable
                 css_template = HtmlTemplate.css_template  # pylint: disable=possibly-unused-variable
                 html_repr = f"{locals()['formatted_html_template']}{locals()['css_template']}"
-        except:  # pylint: disable=bare-except
+        except Exception:
             html_repr = f"<pre>{escape(repr(self))}</pre>"
         return html_repr
 
@@ -200,9 +191,7 @@ class DataSet(Mapping[str, xr.Dataset]):
                 kwargs = {"engine": engine}
                 if compress:
                     kwargs["encoding"] = {
-                        var_name: {"zlib": True}
-                        for var_name, values in data.variables.items()
-                        if _compressible_dtype(values.dtype)
+                        var_name: {"zlib": True} for var_name, values in data.variables.items() if _compressible_dtype(values.dtype)
                     }
                 data.to_netcdf(filename, mode=mode, group=group, **kwargs)
                 data.close()
@@ -218,11 +207,9 @@ class DataSet(Mapping[str, xr.Dataset]):
                 empty_netcdf_file = nc.Dataset(filename, mode="w", format="NETCDF4")
             empty_netcdf_file.close()
         return filename
-    
+
     @staticmethod
-    def from_netcdf(
-        filename, *, engine="h5netcdf", group_kwargs=None, regex=False
-    ) -> "DataSet":
+    def from_netcdf(filename, *, engine="h5netcdf", group_kwargs=None, regex=False) -> "DataSet":
         """Initialize object from a netcdf file.
 
         Expects that the file will have groups, each of which can be loaded by xarray.
@@ -257,14 +244,10 @@ class DataSet(Mapping[str, xr.Dataset]):
         elif engine == "netcdf4":
             import netCDF4 as nc
         else:
-            raise ValueError(
-                f"Invalid value for engine: {engine}. Valid options are: h5netcdf or netcdf4"
-            )
+            raise ValueError(f"Invalid value for engine: {engine}. Valid options are: h5netcdf or netcdf4")
 
         try:
-            with h5netcdf.File(filename, mode="r") if engine == "h5netcdf" else nc.Dataset(
-                filename, mode="r"
-            ) as data:
+            with h5netcdf.File(filename, mode="r") if engine == "h5netcdf" else nc.Dataset(filename, mode="r") as data:
                 data_groups = list(data.groups)
 
             for group in data_groups:
@@ -298,7 +281,6 @@ class DataSet(Mapping[str, xr.Dataset]):
                     )
                 ) from err
             raise err
-        
 
     def extend(self, other, join="left"):
         """Extend InferenceData with groups from another InferenceData.
@@ -358,7 +340,6 @@ class DataSet(Mapping[str, xr.Dataset]):
             setattr(self, group, dataset)
             self._groups.append(group)
 
-
     set_index = _extend_xr_method(xr.Dataset.set_index, see_also="reset_index")
     get_index = _extend_xr_method(xr.Dataset.get_index)
     reset_index = _extend_xr_method(xr.Dataset.reset_index, see_also="set_index")
@@ -384,78 +365,80 @@ class DataSet(Mapping[str, xr.Dataset]):
     sum = _extend_xr_method(xr.Dataset.sum, see_also="cumsum")  # type: ignore[attr-defined]
 
 
-
-
-
-def compute_rhats(data, threshold = 1.001, num_chains = 1):
-    if num_chains == 1 : data = tree_map(lambda x: x[None, ...], data) 
+def compute_rhats(data, threshold=1.001, num_chains=1):
+    if num_chains == 1:
+        data = tree_map(lambda x: x[None, ...], data)
     rhats = []
     for name, value in data.items():
-            value = device_get(value)
-            rhat = split_gelman_rubin(value)
-            if isinstance(rhat, np.ndarray):
-                rhats.extend(rhat)
-            else:
-                rhats.append(rhat)
+        value = device_get(value)
+        rhat = split_gelman_rubin(value)
+        if isinstance(rhat, np.ndarray):
+            rhats.extend(rhat)
+        else:
+            rhats.append(rhat)
     sel = np.array(rhats) < threshold
     tot = len(sel)
     keep_sampling = True
-    percent_not_converged = (sum(~sel)/tot)*100
-    if (sum(~sel)/tot)*100 < 0.1:
+    percent_not_converged = (sum(~sel) / tot) * 100
+    if (sum(~sel) / tot) * 100 < 0.1:
         keep_sampling = False
-        print(f'it is recommended to stop sampling')
+        print("it is recommended to stop sampling")
     else:
-        print(f'it is recommended to continue sampling as {percent_not_converged:.0f}% of parameters have rhat above {threshold}')
+        print(f"it is recommended to continue sampling as {percent_not_converged:.0f}% of parameters have rhat above {threshold}")
 
     return keep_sampling
 
 
-def compute_effective_sample_size(data, threshold = 1000, num_chains = 1):
-    if num_chains == 1 : data = tree_map(lambda x: x[None, ...], data)
+def compute_effective_sample_size(data, threshold=1000, num_chains=1):
+    if num_chains == 1:
+        data = tree_map(lambda x: x[None, ...], data)
     for name, value in data.items():
         n_eff = effective_sample_size(value)
     keep_sampling = True
     if n_eff > threshold:
         keep_sampling = False
-        print(f'it is recommended to stop sampling')
+        print("it is recommended to stop sampling")
     else:
-        print(f'it is recommended to continue sampling as {n_eff:.0f} is below the threshold value of {threshold}')
+        print(f"it is recommended to continue sampling as {n_eff:.0f} is below the threshold value of {threshold}")
     return keep_sampling
 
 
-def checkpoint(kernel, rng_key,  file_name, threshold, statistic = 'rhat', file_path = '', num_warmup = 50000, max_samples = 100000, num_chains = 1, thinning = 1, model_kwargs = {}, mcmc_kwargs = {}):
+def checkpoint(
+    kernel,
+    rng_key,
+    file_name,
+    threshold,
+    statistic="rhat",
+    file_path="",
+    num_warmup=50000,
+    max_samples=100000,
+    num_chains=1,
+    thinning=1,
+    model_kwargs={},
+    mcmc_kwargs={},
+):
 
-    if statistic == 'rhat':
+    if statistic == "rhat":
         statfunc = compute_rhats
 
-    elif statistic == 'n_eff':
+    elif statistic == "n_eff":
         statfunc = compute_effective_sample_size
     else:
-        raise ValueError('only effective sample size (n_eff) and gelman rubin diagnostic (rhat) are supported')
+        raise ValueError("only effective sample size (n_eff) and gelman rubin diagnostic (rhat) are supported")
 
     num_samples = int(max_samples / 10)
     MCMC_RNG, PRIOR_RNG, _RNG = random.split(rng_key, num=3)
 
-    mcmc = MCMC(
-            kernel,
-            thinning=thinning,
-            num_warmup=num_warmup,
-            num_samples=num_samples,
-            num_chains=num_chains,
-            **mcmc_kwargs
-        )
+    mcmc = MCMC(kernel, thinning=thinning, num_warmup=num_warmup, num_samples=num_samples, num_chains=num_chains, **mcmc_kwargs)
 
-    mcmc.run(
-                MCMC_RNG,
-                **model_kwargs
-            )
+    mcmc.run(MCMC_RNG, **model_kwargs)
 
     count = 1
     first_10percent_of_samples = mcmc.get_samples()
     first_10percent_idata = az.from_numpyro(mcmc)
-    DataSet(posterior = first_10percent_idata.posterior).to_netcdf(file_path + file_name + f'_{num_samples}x{count}_dataset.h5')
-    print(f'checkpoint file {count} saved')
-    keep_sampling = statfunc(first_10percent_of_samples, threshold = threshold, num_chains = num_chains)
+    DataSet(posterior=first_10percent_idata.posterior).to_netcdf(file_path + file_name + f"_{num_samples}x{count}_dataset.h5")
+    print(f"checkpoint file {count} saved")
+    keep_sampling = statfunc(first_10percent_of_samples, threshold=threshold, num_chains=num_chains)
 
     while keep_sampling:
         mcmc.post_warmup_state = mcmc.last_state
@@ -463,22 +446,24 @@ def checkpoint(kernel, rng_key,  file_name, threshold, statistic = 'rhat', file_
         next_10percent_of_samples = mcmc.get_samples()
         next_10percent_idata = az.from_numpyro(mcmc)
         count += 1
-        DataSet(posterior = next_10percent_idata.posterior).to_netcdf(file_path + file_name + f'_{num_samples}x{count}_dataset.h5', )
-        print(f'checkpoint file {count} saved')
+        DataSet(posterior=next_10percent_idata.posterior).to_netcdf(
+            file_path + file_name + f"_{num_samples}x{count}_dataset.h5",
+        )
+        print(f"checkpoint file {count} saved")
         if count == 10:
             keep_sampling = False
-            print('number of samples has exceeded max samples, sampling stopped')
+            print("number of samples has exceeded max samples, sampling stopped")
         else:
-            keep_sampling = statfunc(next_10percent_of_samples, threshold = threshold, num_chains = num_chains)
+            keep_sampling = statfunc(next_10percent_of_samples, threshold=threshold, num_chains=num_chains)
 
     if count > 1:
-        dataset = DataSet.from_netcdf(file_path + file_name + f'_{num_samples}x1_dataset.h5')
+        dataset = DataSet.from_netcdf(file_path + file_name + f"_{num_samples}x1_dataset.h5")
         dataset = dataset.posterior
-        for i in np.arange(2,count+1):
-            dat = DataSet.from_netcdf(file_path + file_name + f'_{num_samples}x{i}_dataset.h5')
-            dat.posterior['draw'] = dat.posterior['draw'] + num_samples*(i-1)
+        for i in np.arange(2, count + 1):
+            dat = DataSet.from_netcdf(file_path + file_name + f"_{num_samples}x{i}_dataset.h5")
+            dat.posterior["draw"] = dat.posterior["draw"] + num_samples * (i - 1)
             dataset = xr.merge([dataset, dat.posterior])
-        DataSet(posterior = dataset).to_netcdf(file_path + file_name + f'_{num_samples*count}s_merged{count}_dataset.h5')
+        DataSet(posterior=dataset).to_netcdf(file_path + file_name + f"_{num_samples*count}s_merged{count}_dataset.h5")
 
         return dataset
 
