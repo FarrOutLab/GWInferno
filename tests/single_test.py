9c323029f6d63f68d049891696b709759761ca16
Jaxen Godfrey <jaxeng@uoregon.edu>
2024-06-27 13:08:03 -0700

    add to single test

--- tests/single_test.py
+++ tests/single_test.py
@@ -3,12 +3,21 @@ import unittest
 
 import jax.numpy as jnp
 import xarray as xr
+from astropy.cosmology import Planck15
 
 from gwinferno.models.bsplines.single import Base1DBSplineModel
+from gwinferno.models.bsplines.single import BSplineChiEffective
+from gwinferno.models.bsplines.single import BSplineChiPrecess
+from gwinferno.models.bsplines.single import BSplineMass
+from gwinferno.models.bsplines.single import BSplineRatio
+from gwinferno.models.bsplines.single import BSplineRedshift
+from gwinferno.models.bsplines.single import BSplineSpinMagnitude
+from gwinferno.models.bsplines.single import BSplineSpinTilt
+from gwinferno.models.bsplines.single import BSplineSymmetricChiEffective
 from gwinferno.preprocess.data_collection import load_injections
 
 
-class TestBase1DBsplineModel(unittest.TestCase):
+class TestBase1DBSplineModel(unittest.TestCase):
     def setUp(self) -> None:
         if os.path.exists("~/unit_tests/data"):
             pref = "~/unit_tests/data"
@@ -29,7 +38,7 @@ class TestBase1DBsplineModel(unittest.TestCase):
         del self.nsplines
 
     def load_injections(self, **kwargs):
-        p_names = ["mass_1"]
+        p_names = ["mass_1", "mass_ratio", "redshift", "a_1", "a_2", "cos_tilt_1", "cos_tilt_2"]
         injections = load_injections(self.inj_file, p_names, through_o3=kwargs["through_o3"], through_o4a=kwargs["through_o4a"])
         injdata = jnp.asarray(injections.data)
         injdict = {k: injdata[i] for i, k in enumerate(injections.param.values)}
@@ -42,12 +51,31 @@ class TestBase1DBsplineModel(unittest.TestCase):
         pedict = {k: pedata[:, i, :] for i, k in enumerate(dataarray.param.values)}
         return pedict
 
-    def test_spline_shape(self):
-        pe_x = self.pedict["mass_1"]
-        inj_x = self.injdict["mass_1"]
-        bspline = Base1DBSplineModel(self.nsplines, pe_x, inj_x)
+    def spline_shape(self, model, pe_x, inj_x, redshift=False):
 
-        inj_pdf = bspline.funcs[0](self.coefs)
-        pe_pdf = bspline.funcs[1](self.coefs)
+        if redshift:
+            pe_dvcdz = jnp.array(Planck15.differential_comoving_volume(pe_x).value * 4 * jnp.pi)
+            inj_dvcdz = jnp.array(Planck15.differential_comoving_volume(inj_x).value * 4 * jnp.pi)
+            bspline = model(self.nsplines, pe_x, inj_x, pe_dvcdz, inj_dvcdz)
+            pe_pdf = bspline(self.coefs, pe_samples=True)
+            inj_pdf = bspline(self.coefs, pe_samples=False)
+
+        else:
+            bspline = model(self.nsplines, pe_x, inj_x)
+            pe_pdf = bspline(self.coefs, pe_samples=True)
+            inj_pdf = bspline(self.coefs, pe_samples=False)
+
         self.assertEqual(pe_x.shape, pe_pdf.shape, msg="PE sample shape different than PE PDF shape")
         self.assertEqual(inj_x.shape, inj_pdf.shape, msg="Inj sample shape different than Inj PDF shape")
+
+    def test_functions(self):
+        self.spline_shape(BSplineSpinMagnitude, self.pedict["a_1"], self.injdict["a_1"])
+        self.spline_shape(Base1DBSplineModel, self.pedict["mass_1"], self.injdict["mass_1"])
+        self.spline_shape(BSplineSpinTilt, self.pedict["cos_tilt_1"], self.injdict["cos_tilt_1"])
+        self.spline_shape(BSplineChiEffective, self.pedict["cos_tilt_1"], self.injdict["cos_tilt_1"])
+        self.spline_shape(BSplineChiEffective, self.pedict["cos_tilt_1"], self.injdict["cos_tilt_1"])
+        self.spline_shape(BSplineSymmetricChiEffective, self.pedict["cos_tilt_1"], self.injdict["cos_tilt_1"])
+        self.spline_shape(BSplineChiPrecess, self.pedict["a_1"], self.injdict["a_1"])
+        self.spline_shape(BSplineRatio, self.pedict["mass_ratio"], self.injdict["mass_ratio"])
+        self.spline_shape(BSplineMass, self.pedict["mass_1"], self.injdict["mass_1"])
+        self.spline_shape(BSplineRedshift, self.pedict["redshift"], self.injdict["redshift"], redshift=True)
