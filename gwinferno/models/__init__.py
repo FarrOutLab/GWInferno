8bec86cb81a5ab04e8eae1a5a9e4c6acf62c71e5
Jaxen Godfrey <jaxeng@uoregon.edu>
2024-07-04 12:50:26 -0700

    precommit

--- gwinferno/models/__init__.py
+++ gwinferno/models/__init__.py
@@ -3,6 +3,6 @@ a module that stores defined population models
 """
 
 from . import bsplines
-from . import parametric
 from . import mass_gap
+from . import parametric
 from . import spline_perturbation
