b3cc067731d3565896093edc149ce99ce5d2772b
Ben Farr <bfarr@uoregon.edu>
2024-07-17 09:41:44 +0200

    improve docs and add tests for preprocess.conversions

--- gwinferno/preprocess/conversions.py
+++ gwinferno/preprocess/conversions.py
@@ -6,78 +6,105 @@ import numpy as np
 
 
 def chieff_from_q_component_spins(q, a1, a2, ct1, ct2):
-    """
-    chieff_from_q_component_spins calculates the effective spin paramer given the
-        mass ratio and spin mag/tilts.
+    r"""Calculate the effective spin from, mass ratio, component spin magnitudes
+    and tilts.
 
-    Args:
-        q (array_like): mass ratio (defined from 0 to 1)
-        a1 (array_like): primary spin magnitude
-        a2 (array_like): secondary spin magnitude
-        ct1 (array_like): primary cos tilt angle
-        ct2 (array_like): secondary cos tilt angle
+    .. math::
+        \chi_\mathrm{eff} = \frac{a_1 \cos t_1 + q a_2 \cos \theta_2}{1 + q}
 
-    Returns:
-        array_like: effective spin
+    Parameters
+    ----------
+    q : array_like
+        Mass ratio (defined from 0 to 1).
+    a1 : array_like
+        Spin magnitude of the primary component.
+    a2 : array_like
+        Spin magnitude of the secondary component.
+    ct1 : array_like
+        Cosine of the spin tilt of the primary component.
+    ct2 : array_like
+        Cosine of the spin tilt of the secondary component.
+
+    Returns
+    -------
+    array_like
+        Effective spin.
     """
     return (a1 * ct1 + q * a2 * ct2) / (1.0 + q)
 
 
-def chip_from_q_component_spins(q, a1, a2, ct1, ct2, bk=np):
+def chip_from_q_component_spins(q, a1, a2, ct1, ct2, math=np):
+    """Calculate the effective precessing spin, :math:`\chi_\mathrm{p}`, from
+    the mass ratio, component spin magnitudes and tilts.
+
+    Parameters
+    ----------
+    q : array_like
+        Mass ratio (defined from 0 to 1).
+    a1 : array_like
+        Spin magnitude of the primary component.
+    a2 : array_like
+        Spin magnitude of the secondary component.
+    ct1 : array_like
+        Cosine of the spin tilt of the primary component.
+    ct2 : array_like
+        Cosine of the spin tilt of the secondary component.
+    math : module, default=`numpy`
+        Math module to use.
+
+    Returns
+    -------
+    array_like
+        Effective precessing spin.
     """
-    chip_from_q_component_spins calculates the effective spin precession paramer given
-        the mass ratio and spin mag/tilts.
+    sint1 = math.sqrt(1.0 - ct1**2)
+    sint2 = math.sqrt(1.0 - ct2**2)
+    return math.maximum(a1 * sint1, ((3.0 + 4.0 * q) / (4.0 + 3.0 * q)) * q * a2 * sint2)
 
-    Args:
-        q (array_like): mass ratio (defined from 0 to 1)
-        a1 (array_like): primary spin magnitude
-        a2 (array_like): secondary spin magnitude
-        ct1 (array_like): primary cos tilt angle
-        ct2 (array_like): secondary cos tilt angle
 
-    Returns:
-        array_like: effective spin precession
+def mu_var_from_alpha_beta(alpha, beta, xmax=1):
+    """Convert α, β shape parameters of the Beta distribution to the mean and variance.
+
+    Parameters
+    ----------
+    alpha : array_like
+        α shape parameter.
+    beta : array_like
+        β shape parameter.
+    xmax : array_like, default=1
+        Maximum value with support.
+
+    Returns
+    -------
+    array_like, array_like
+        Tuple of arrays with shape matching inputs that represent the
+        means and variances corresponding to the shape parameters.
     """
-    sint1 = bk.sqrt(1.0 - ct1**2)
-    sint2 = bk.sqrt(1.0 - ct2**2)
-    return bk.maximum(a1 * sint1, ((3.0 + 4.0 * q) / (4.0 + 3.0 * q)) * q * a2 * sint2)
-
-
-def mu_var_from_alpha_beta(alpha, beta, amax=1):
-    """
-    mu_var_from_alpha_beta converts the alpha/beta shape parameters of the Beta
-        Distribution to the mean and variance (mu/var)
-
-    Args:
-        alpha (array_like): alpha Beta distribution shape parameter
-        beta (array_like): beta Beta distribution shape parameter
-        amax (array_like, optional): maximum spin mag. Defaults to 1.
-
-    Returns:
-        (array_like, array_like): returns tuple of arrays same shape as input that
-            represents the mean and variances given shape parameters
-    """
-    mu = alpha / (alpha + beta) * amax
-    var = alpha * beta / ((alpha + beta) ** 2 * (alpha + beta + 1)) * amax**2
+    mu = alpha / (alpha + beta) * xmax
+    var = alpha * beta / ((alpha + beta) ** 2 * (alpha + beta + 1)) * xmax**2
     return mu, var
 
 
-def alpha_beta_from_mu_var(mu, var, amax=1):
-    """
-    alpha_beta_from_mu_var converts the mean/variance (mu/var) parameters of the Beta
-        Distribution to the shape parameters (alpha/beta)
+def alpha_beta_from_mu_var(mu, var, xmax=1):
+    """Convert desired mean and variance to α, β shape parameters of the Beta distribution.
 
-    Args:
-        mu (array_like): mean of Beta distribution
-        var (array_like): variance of Beta distribution
-        amax (array_like, optional): maximum spin mag. Defaults to 1.
+    Parameters
+    ----------
+    mean : array_like
+        Desired mean.
+    var : array_like
+        Desired variance.
+    xmax : array_like, default=1
+        Maximum value with support.
 
-    Returns:
-        (array_like, array_like): returns tuple of arrays same shape as input that
-            represents the alpha and beta shape parameters given mean and variance
+    Returns
+    -------
+    array_like, array_like
+        Tuple of arrays with shape matching inputs that represent the
+        α, β shape parameters corresponding to the desired means and variances.
     """
-    mu /= amax
-    var /= amax**2
+    mu /= xmax
+    var /= xmax**2
     alpha = (mu**2 * (1 - mu) - mu * var) / var
     beta = (mu * (1 - mu) ** 2 - (1 - mu) * var) / var
     return alpha, beta
