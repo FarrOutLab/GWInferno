d9d98c3d10a9590905ebb95a60b44d437e942938
Ivan Juarez-Reyes <ijuarez.ln@gmail.com>
2025-03-21 12:53:36 -0700

    Moved BivariateBSpline to interpolation.py

--- gwinferno/interpolation.py
+++ gwinferno/interpolation.py
@@ -638,3 +638,56 @@ class BSplines():
         assert coeffs.shape[0] == design_matrix.shape[0], "The number of coefficients must match the number of B-splines."
         spline = jnp.einsum('ij,i->j', design_matrix, coeffs)
         return spline * self.normalize(coeffs)
+
+class BivariateBSpline():
+
+    def __init__(self,
+                 u_domain, v_domain,
+                 u_order = int, v_order = int,
+                 u_l = int, v_l = int,
+                 u_knots=None, v_knots=None,
+                 normalization=True):
+                 # TODO: Add argument for class to use when setting up B-spline?
+        """Base class to construct the 2D B-spline design tensor.
+
+        Args:
+            u_domain (array-like): lower and upper values for the u domain of interest
+            v_domain (array-like): lower and upper values for the v domain of interest
+            u_order (int): order of B-spline in the u-direction
+            v_order (int): order of B-spline in the v-direction
+            u_l (int): u_l+1 number of B-splines
+            v_l (int): v_l+1 number of B-splines
+            u_knots (array-like, optional): knot vector in the u-direction (both exterior and interior knots)
+            v_knots (array-like, optional): knot vector in the v-direction (both exterior and interior knots)
+            normalization (bool, optional): flag to numerically normalize the spline.
+            independent (bool, optional): flag to construct 2D B-splines with all points (True), or pairs of points (False)
+        """
+        self.domain = np.array([u_domain, v_domain])
+        self.ls = np.array([u_l, v_l])
+        self.orders = np.array([u_order, v_order])
+        self.degrees = np.array([u_order - 1, v_order - 1])
+        self.normalization = normalization
+        # TODO: Modify BSplines class that is called?
+        self.u_BSpline = BSplines(u_domain = self.domain[0], P = self.orders[0], l = self.ls[0], knots = u_knots, normalization=self.normalization)
+        self.v_BSpline = BSplines(u_domain = self.domain[1], P = self.orders[1], l = self.ls[1], knots = v_knots, normalization=self.normalization)
+
+    def design_tensor(self, u_domain, v_domain, independent=False):
+        u_BSpline_dm = self.u_BSpline.design_matrix(u_domain)
+        v_BSpline_dm = self.v_BSpline.design_matrix(v_domain)
+        if independent:
+            return np.tensordot(u_BSpline_dm, v_BSpline_dm, axes = 0)
+        else:
+            uv_BSpline = np.empty((self.u_BSpline.l_E + 1, self.v_BSpline.l_E + 1, u_BSpline_dm.shape[1]))
+            # TODO: Figure out how to not use a nested for-loop to perform this operation 
+            for s in range(self.v_BSpline.l_E + 1):
+                for k in range(self.u_BSpline.l_E + 1):
+                    uv_BSpline = uv_BSpline.at[k,s,:].set(u_BSpline_dm[k,:] * v_BSpline_dm[s,:])
+            return uv_BSpline
+    
+    def spline(self, coeffs, design_tensor, independent=False):
+        if independent:
+            assert coeffs.shape[0] == design_tensor.shape[0] and coeffs.shape[1] == design_tensor.shape[2], "The number of coefficients must match the number of B-splines."
+            return jnp.einsum('kisj,ks->ij', design_tensor, coeffs)
+        else:
+            assert coeffs.shape[0] == design_tensor.shape[0] and coeffs.shape[1] == design_tensor.shape[1], "The number of coefficients must match the number of B-splines."
+            return jnp.einsum('ksj,ks->j', design_tensor, coeffs)
