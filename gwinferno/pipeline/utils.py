412e5477eced930b61f68bfa9c444c93a413c4c7
Ivan Juarez-Reyes <ijuarez.ln@gmail.com>
2025-07-01 12:55:45 -0700

    Split up setup_bspline_spin_models and bspline_spin_prior for 2d model

--- gwinferno/pipeline/utils.py
+++ gwinferno/pipeline/utils.py
@@ -16,9 +16,11 @@ from gwinferno.models.bsplines.separable import BSplineIndependentSpinTilts
 from gwinferno.models.bsplines.separable import BSplinePrimaryBSplineRatio
 from gwinferno.models.bsplines.smoothing import apply_difference_prior
 from gwinferno.models.spline_perturbation import PowerlawSplineRedshiftModel
-from gwinferno.interpolation import LogYBSplines
-from gwinferno.models.bsplines.single import Base1DBSplineModel_IJR
+from gwinferno.models.bsplines.separable import BSplineIndependentSpinTilts_IJR
 from gwinferno.models.bsplines.joint import Base2DBSplineModel
+from gwinferno.models.bsplines.joint import BivariateBSplineSpinMag
+from gwinferno.models.bsplines.joint import BivariateBSplineSpinTilt
+from gwinferno.models.bsplines.separable import BivariateBSplineIIDSpinMagTilt
 
 
 def load_base_parser():
@@ -121,8 +123,7 @@ def setup_bspline_mass_models(pedict, injdict, m_nsplines, q_nsplines, mmin, mma
     )
 
 
-def setup_bspline_spin_models(pedict, injdict, a1_nsplines, ct1_nsplines, IID=False, a2_nsplines=None, ct2_nsplines=None, bivariate=True):
-    # TODO: move the ```Bivariate``` flag before the number of B-splines arguments and set default to False - set to True for testing
+def setup_bspline_spin_models(pedict, injdict, a1_nsplines, ct1_nsplines, IID=False, a2_nsplines=None, ct2_nsplines=None):
     print("initializing spline spin design matrices")
 
     if IID:
@@ -132,32 +133,34 @@ def setup_bspline_spin_models(pedict, injdict, a1_nsplines, ct1_nsplines, IID=Fa
 
         mag_model = BSplineIIDSpinMagnitudes(a1_nsplines, pedict["a_1"], pedict["a_2"], injdict["a_1"], injdict["a_2"], normalize=True)
 
-    elif bivariate:
-        tilt_model = Base2DBSplineModel(u_pe_vals = pedict["cos_tilt_1"], u_inj_vals = injdict["cos_tilt_1"], v_pe_vals = pedict["cos_tilt_2"],
-            v_inj_vals = injdict["cos_tilt_2"], u_domain=(-1.0, 1.0), v_domain=(-1.0, 1.0), ul_BSplines = ct1_nsplines, vl_BSplines = ct2_nsplines,
-            normalization = True)
-
-        mag_model = Base2DBSplineModel(u_pe_vals = pedict["a_1"], u_inj_vals = injdict["a_1"], v_pe_vals = pedict["a_2"],
-            v_inj_vals = injdict["a_2"], ul_BSplines = a1_nsplines, vl_BSplines = a2_nsplines,
-            normalization = True)
-
     else:
-        tilt_model = BSplineIndependentSpinTilts(
-            ct1_nsplines,
-            ct2_nsplines,
-            pedict["cos_tilt_1"],
-            pedict["cos_tilt_2"],
-            injdict["cos_tilt_1"],
-            injdict["cos_tilt_2"],
-            normalize=True,
-        )
-
+        tilt_model = BSplineIndependentSpinTilts_IJR(u_pe_vals=pedict["cos_tilt_1"], u_inj_vals=injdict["cos_tilt_1"],
+                                                     v_pe_vals=pedict["cos_tilt_2"], v_inj_vals=injdict["cos_tilt_2"],
+                                                     ul_BSplines=ct1_nsplines, vl_BSplines=ct2_nsplines,
+                                                     normalization=True, book=True)
+        # tilt_model = BSplineIndependentSpinTilts(
+        #     ct1_nsplines,
+        #     ct2_nsplines,
+        #     pedict["cos_tilt_1"],
+        #     pedict["cos_tilt_2"],
+        #     injdict["cos_tilt_1"],
+        #     injdict["cos_tilt_2"],
+        #     normalize=True,
+        # )
         mag_model = BSplineIndependentSpinMagnitudes(
             a1_nsplines, a2_nsplines, pedict["a_1"], pedict["a_2"], injdict["a_1"], injdict["a_2"], normalize=True
         )
-
     return mag_model, tilt_model
 
+def setup_bspline_spin_models_2d(pedict, injdict, a1_nsplines, ct1_nsplines, IID=False, a2_nsplines=None, ct2_nsplines=None):
+    print("initializing spline spin design tensors")
+
+    if IID:
+        spin_mag_tilt_model = BivariateBSplineIIDSpinMagTilt(ndofs=(a1_nsplines, ct1_nsplines),
+                                    primary_pe_vals=(pedict["a_1"], pedict["cos_tilt_1"]), primary_inj_vals=(injdict["a_1"], injdict["cos_tilt_1"]),
+                                    secondary_pe_vals=(pedict["a_2"], pedict["cos_tilt_2"]), secondary_inj_vals=(injdict["a_2"], injdict["cos_tilt_2"]),
+                                    normalize=True)
+    return spin_mag_tilt_model
 
 def setup_powerlaw_spline_redshift_model(pedict, injdict, z_nsplines):
     print("initializing redshift model")
@@ -173,7 +176,7 @@ Setup B-Spline Priors
 """
 
 
-def bspline_mass_prior(m_nsplines=None, q_nsplines=None, m_tau=1, q_tau=1, name=None, m_cs_sig=15, q_cs_sig=5, m_deg=1, q_deg=1):
+def bspline_mass_prior(m_nsplines=None, q_nsplines=None, m_tau=1, q_tau=1, name=None, m_cs_sig=15, q_cs_sig=5, m_deg=2, q_deg=2):
 
     name = "_" + name if name is not None else ""
 
@@ -195,9 +198,7 @@ def bspline_mass_prior(m_nsplines=None, q_nsplines=None, m_tau=1, q_tau=1, name=
         return mass_cs, q_cs
 
 
-def bspline_spin_prior(a_nsplines=None, ct_nsplines=None, a_tau=None, ct_tau=None, name=None, IID=False, a_cs_sig=5, ct_cs_sig=5, a_deg=2, ct_deg=2,
-    Bivariate=True):
-    # TODO: move the ```Bivariate``` flag before the number of B-splines arguments and set default to False - set to True for testing
+def bspline_spin_prior(a_nsplines=None, ct_nsplines=None, a_tau=None, ct_tau=None, name=None, IID=False, a_cs_sig=5, ct_cs_sig=5, a_deg=2, ct_deg=2):
 
     name = "_" + name if name is not None else ""
 
@@ -210,29 +211,28 @@ def bspline_spin_prior(a_nsplines=None, ct_nsplines=None, a_tau=None, ct_tau=Non
         return a_cs, ct_cs
 
     else:
-        if Bivariate:
-            a_cs = numpyro.sample("a_cs" + name, dist.Normal(0, a_cs_sig), sample_shape=(a_nsplines, a_nsplines))
-            numpyro.factor("a_smoothing_prior" + name, apply_difference_prior(a_cs, a_tau, degree=a_deg))
-            
-            ct_cs = numpyro.sample("tilt_cs" + name, dist.Normal(0, ct_cs_sig), sample_shape=(ct_nsplines, ct_nsplines))
-            numpyro.factor("ct_smoothing_prior" + name, apply_difference_prior(ct_cs, ct_tau, degree=ct_deg))
-            print('Bivariate: yes')
-            # print('a_cs shape: ', a_cs.shape)
-            # print('ct_cs shape: ', ct_cs.shape)
-            return a_cs, ct_cs
-        else:
-            a1_cs = numpyro.sample("a1_cs" + name, dist.Normal(0, a_cs_sig), sample_shape=(a_nsplines,))
-            numpyro.factor("a1_smoothing_prior" + name, apply_difference_prior(a1_cs, a_tau, degree=a_deg))
-            a2_cs = numpyro.sample("a2_cs" + name, dist.Normal(0, a_cs_sig), sample_shape=(a_nsplines,))
-            numpyro.factor("a2_smoothing_prior" + name, apply_difference_prior(a2_cs, a_tau, degree=a_deg))
+        a1_cs = numpyro.sample("a1_cs" + name, dist.Normal(0, a_cs_sig), sample_shape=(a_nsplines,))
+        numpyro.factor("a1_smoothing_prior" + name, apply_difference_prior(a1_cs, a_tau, degree=a_deg))
+        a2_cs = numpyro.sample("a2_cs" + name, dist.Normal(0, a_cs_sig), sample_shape=(a_nsplines,))
+        numpyro.factor("a2_smoothing_prior" + name, apply_difference_prior(a2_cs, a_tau, degree=a_deg))
 
-            ct1_cs = numpyro.sample("tilt1_cs" + name, dist.Normal(0, ct_cs_sig), sample_shape=(ct_nsplines,))
-            numpyro.factor("ct1_smoothing_prior" + name, apply_difference_prior(ct1_cs, ct_tau, degree=ct_deg))
-            ct2_cs = numpyro.sample("tilt2_cs" + name, dist.Normal(0, ct_cs_sig), sample_shape=(ct_nsplines,))
-            numpyro.factor("ct2_smoothing_prior" + name, apply_difference_prior(ct2_cs, ct_tau, degree=ct_deg))
-            print('IID: no')
-            return a1_cs, ct1_cs, a2_cs, ct2_cs
+        ct1_cs = numpyro.sample("tilt1_cs" + name, dist.Normal(0, ct_cs_sig), sample_shape=(ct_nsplines,))
+        numpyro.factor("ct1_smoothing_prior" + name, apply_difference_prior(ct1_cs, ct_tau, degree=ct_deg))
+        ct2_cs = numpyro.sample("tilt2_cs" + name, dist.Normal(0, ct_cs_sig), sample_shape=(ct_nsplines,))
+        numpyro.factor("ct2_smoothing_prior" + name, apply_difference_prior(ct2_cs, ct_tau, degree=ct_deg))
+        print('IID: no')
+        return a1_cs, ct1_cs, a2_cs, ct2_cs
+        
+def bspline_spin_prior_2d(a_nsplines=None, ct_nsplines=None, a_tau=None, ct_tau=None, name=None, IID=False, a_cs_sig=5, ct_cs_sig=5, a_deg=2, ct_deg=2):
 
+    name = "_" + name if name is not None else ""
+    print('Bivariate: yes')
+    if IID:
+        # TODO: ct and a sigs, degs, and taus are the same in `basic_bspline_example_IJR`, so we choose one to set for the sample below.
+        # Eventually, want to make this separate from either ct and a, as its own thing!
+        a_ct_cs = numpyro.sample("a_ct_cs" + name, dist.Dirichlet(jnp.ones((a_nsplines,ct_nsplines), dtype='float32')))
+        # numpyro.factor("a_ct_smoothing_prior" + name, apply_difference_prior(a_ct_cs, a_tau, degree=a_deg))
+        return a_ct_cs
 
 def bspline_redshift_prior(z_nsplines=None, z_tau=None, name=None, z_cs_sig=1, z_deg=2):
     name = "_" + name if name is not None else ""
