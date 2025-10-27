92241cd5e079c429c59c0f8aa43ba1914a79686c
Ivan Juarez-Reyes <ijuarez.ln@gmail.com>
2025-10-27 11:35:44 -0700

    Added 2D B-spline difference prior

--- gwinferno/models/bsplines/smoothing.py
+++ gwinferno/models/bsplines/smoothing.py
@@ -26,3 +26,16 @@ def apply_difference_prior(coefs, inv_var, degree=1):
     delta_c = jnp.diff(coefs, n=degree)
     prior = -0.5 * inv_var * jnp.dot(delta_c, delta_c.T)
     return prior
+
+def apply_2d_difference_prior(coeffs, inv_var_row, inv_var_column, order=1):
+    """Computes the P-spline difference penalty
+
+    Args:
+        coeffs (array-like): coefficients of the B-spline
+        inv_var (float): inverse of the penalty tuning parameter. Higher values result in smoother functions
+        order (int, default=1): difference order
+    """
+    delta_r_squared = jnp.sum(jnp.square(jnp.diff(coeffs, n=order, axis=-1)))
+    delta_c_squared = jnp.sum(jnp.square(jnp.diff(coeffs, n=order, axis=-2)))
+    prior = -0.5 * (inv_var_row * delta_r_squared + inv_var_column * delta_c_squared)
+    return prior
