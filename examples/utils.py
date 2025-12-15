765c4ecd944179e23b94e5abff0e09d504a8374a
Ivan Juarez-Reyes <ijuarez.ln@gmail.com>
2025-12-15 14:29:49 -0800

    Added trace plots to bspline_analysis, added powerlaw_peak_bbspline analysis

--- examples/utils.py
+++ examples/utils.py
@@ -13,6 +13,7 @@ from gwinferno.pipeline.utils import setup_bspline_spin_models_2d
 from gwinferno.pipeline.utils import setup_bspline_primary_massratio_chieff_models
 from gwinferno.pipeline.utils import setup_powerlaw_spline_redshift_model
 
+from gwinferno.models.bsplines.joint import BivariateBSplineMassRatioChiEff
 from gwinferno.models.parametric.parametric import PowerlawRedshiftModel
 
 
@@ -87,8 +88,9 @@ def run_bspline_analysis(numpyro_model, pedict, injdict, constants, param_names,
         )
         mcmc.print_summary()
         posterior = mcmc.get_samples()
+        trace_plots = az.plot_trace(mcmc, compact=True)
 
-        return posterior, z_model
+        return posterior, z_model, trace_plots
 
     else:
         return z_model
@@ -150,7 +152,7 @@ def run_bspline_analysis_2d(numpyro_model, pedict, injdict, constants, param_nam
     else:
         return z_model
     
-def run_massratio_chieff_bspline_analysis_2d(numpyro_model, pedict, injdict, constants, param_names, nspline_dict, parsargs, hyper_params_dict, skip_inference=False):
+def run_massratio_chieff_bspline_analysis_2d(numpyro_model, pedict, injdict, constants, param_names, nspline_dict, hyper_params_dict, parsargs, skip_inference=False):
     """run MCMC
     
     Args:
@@ -160,6 +162,7 @@ def run_massratio_chieff_bspline_analysis_2d(numpyro_model, pedict, injdict, con
         constants (dict): dictionary of relevant constants
         param_names (list of strs): list of parameters
         nspline_dict (dict): dictionary containing the number of splines for each parameter
+        hyper_params_dict (dict): dictionary containing hyperparameters
         parsargs (ArgumentParser): args from ArgumentParser.parse_args()
         skip_inference (bool, optional): If True, does not perform inference. Defaults to False.
 
@@ -170,7 +173,7 @@ def run_massratio_chieff_bspline_analysis_2d(numpyro_model, pedict, injdict, con
         if skip_inference == True:
             z_model
     """
-    primary_chiq_model = setup_bspline_primary_massratio_chieff_models(pedict, injdict, nspline_dict["m1"], nspline_dict["chi_eff"], nspline_dict["q"],
+    primary_model, chiq_model = setup_bspline_primary_massratio_chieff_models(pedict, injdict, nspline_dict["m1"], nspline_dict["chi_eff"], nspline_dict["q"],
                                                                      mmin=parsargs.mmin, mmax=parsargs.mmax)
     z_model = setup_powerlaw_spline_redshift_model(pedict, injdict, nspline_dict["redshift"])
 
@@ -190,14 +193,77 @@ def run_massratio_chieff_bspline_analysis_2d(numpyro_model, pedict, injdict, con
             constants["nObs"],
             constants["obs_time"],
             constants["total_inj"],
-            primary_chiq_model,
+            primary_model,
+            chiq_model,
             z_model,
             parsargs.mmin,
             parsargs.mmax,
             nspline_dict,
-            param_names,
             hyper_params_dict,
+            param_names,
         )
+        mcmc.print_summary()
+        posterior = mcmc.get_samples()
+        trace_plots = az.plot_trace(mcmc, compact=True)
+
+        return mcmc, posterior, z_model, trace_plots
+    
+    else:
+        return z_model
+    
+def run_powerlaw_peak_bbspline_chiq_analysis(numpyro_model, pedict, injdict, constants, param_names, nspline_dict, hyper_params_dict, parsargs, skip_inference=False):
+    """run MCMC
+    
+    Args:
+        numpyro_model (func): numpyro model that defines priors, population model, and likelihood
+        pedict (dict): dictionary of PE samples
+        injdict (dict): dictionary of injecitons
+        constants (dict): dictionary of relevant constants
+        param_names (list of strs): list of parameters
+        nspline_dict (dict): dictionary containing the number of splines for each parameter
+        hyper_params_dict (dict): dictionary containing hyperparameters
+        parsargs (ArgumentParser): args from ArgumentParser.parse_args()
+        skip_inference (bool, optional): If True, does not perform inference. Defaults to False.
+
+    Returns:
+        if skip_inference == False:
+            posterior (dict): dictionary of posterior samples
+            z_model (obj): redshift model (needed for later calculations)
+        if skip_inference == True:
+            z_model
+    """
+
+    chiq_model = BivariateBSplineMassRatioChiEff(ndofs=(nspline_dict['chi_eff'], nspline_dict['q']),
+                                                 pe_vals=(pedict['chi_eff'], pedict['mass_ratio']),
+                                                 inj_vals=(injdict['chi_eff'], injdict['mass_ratio']),
+                                                 q_min=parsargs.mmin/parsargs.mmax, orders=hyper_params_dict['chiq_order'])
+    z_model = PowerlawRedshiftModel(pedict['redshift'], injdict['redshift'])
+
+    if not skip_inference:
+        nChains = parsargs.chains
+        numpyro.set_host_device_count(nChains)
+        kernel = NUTS(numpyro_model)
+        mcmc = MCMC(kernel, num_warmup=parsargs.warmup, num_samples=parsargs.samples, num_chains=nChains)
+
+        rng_key = random.PRNGKey(parsargs.rngkey)
+        rng_key, catkey, rng_key_ = random.split(rng_key, num=3)
+
+        with numpyro.validation_enabled():
+            mcmc.run(
+                rng_key_,
+                pedict,
+                injdict,
+                constants["nObs"],
+                constants["obs_time"],
+                constants["total_inj"],
+                chiq_model,
+                z_model,
+                parsargs.mmin,
+                parsargs.mmax,
+                nspline_dict,
+                hyper_params_dict,
+                param_names,
+            )
         mcmc.print_summary()
         posterior = mcmc.get_samples()
         trace_plots = az.plot_trace(mcmc, compact=True)
