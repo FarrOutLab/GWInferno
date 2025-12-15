c1b5fd3f2755ef206942f92c2373c5d767e1d61d
Ivan Juarez-Reyes <ijuarez.ln@gmail.com>
2025-12-15 14:46:38 -0800

    Added rcParams update for TeX font, added plot_pdf_axes for custom axes in 1D plots,  swapped u and v args of plot_2dpdf to meshgrid u and v, renamed some functions, added shared_cbar to 2dpercentiles, added marginal chiq pdfs and slices of chiq pdfs

--- gwinferno/postprocess/plot.py
+++ gwinferno/postprocess/plot.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as pltcolors
 import numpy as np
 
+plt.rcParams.update({"text.usetex": True, "font.family": "cmr10", "font.size": 20,
+                     "axes.labelpad": 8, "axes.titlepad": 8})
 
 def plot_pdf(x, pdf, label, color="blue", loglog=True, alpha=1.0):
     med = np.median(pdf, axis=0)
@@ -14,34 +16,38 @@ def plot_pdf(x, pdf, label, color="blue", loglog=True, alpha=1.0):
         plt.plot(x, med, lw=2, color=color, label=label, alpha=alpha)
 
     plt.fill_between(x, low, high, color=color, alpha=0.1)
+    plt.grid(ls='--', alpha=alpha/2)
 
-# def plot_2dpdf(ax1, ax2, u, v, pdf, label, color = "blue", loglog = False, alpha = 1.0):
-#     rng = np.random.default_rng()
-#     rand = pdf[rng.integers(pdf.shape[0])]
-#     U, V = np.meshgrid(u, v, indexing = 'ij')
-#     wireframe = ax1.plot_wireframe(U, V, rand, color = color, alpha = alpha/2)
-#     filled_contour = ax2.contourf(U, V, rand, cmap = 'viridis', alpha = alpha)
-#     ax2.grid(ls='--', alpha=alpha/2)
-#     return wireframe, filled_contour
+def plot_pdf_axes(ax, u, pdf, alpha=1.0, color="blue", label=None, loglog=True):
+    med = np.median(pdf, axis=0)
+    low = np.percentile(pdf, 5, axis=0)
+    high = np.percentile(pdf, 95, axis=0)
 
-def plot_2dpdf(ax1, u, v, pdf, alpha = 1.0, cmap:str='viridis', norm:str='linear'):
-    U, V = np.meshgrid(u, v, indexing = 'ij')
-    cf = ax1.contourf(U, V, pdf, cmap = cmap, alpha = alpha, norm=norm, levels=20)
-    ax1.grid(ls='--', alpha=alpha/2)
+    if loglog:
+        ax.loglog(u, med, lw=2, alpha=alpha, color=color, label=label)
+    else:
+        ax.plot(u, med, lw=2, alpha=alpha, color=color, label=label)
+    
+    ax.fill_between(u, low, high, alpha=alpha/10, color=color)
+    ax.grid(ls='--', alpha=alpha/2)
+
+def plot_2dpdf(ax, u, v, pdf, alpha = 1.0, cmap:str='viridis', norm='log', levels=1000):
+    cf = ax.contourf(u, v, pdf, alpha=alpha, cmap=cmap, norm=norm, levels=levels)
+    ax.grid(ls='--', alpha=alpha/2)
     return cf
 
-def plot_2dstats(ax1, ax2, u, v, pdf, label, alpha = 1.0):
+def plot_2dpercentiles(ax10, ax90, u, v, pdf, alpha = 1.0, cmap:str='viridis', norm='linear', levels=1000, shared_cbar:bool=True):
     low = np.percentile(pdf, 10, axis = 0)
     high = np.percentile(pdf, 90, axis = 0)
-    vmin = min((low.min(), high.min()))
-    vmax = max((low.max(), high.max()))
-    norm = pltcolors.Normalize(vmin, vmax)
+    if shared_cbar:
+        vmin = min((low.min(), high.min()))
+        vmax = max((low.max(), high.max()))
+        norm = pltcolors.Normalize(vmin, vmax)
+    else:
+        norm = norm
 
-    U, V = np.meshgrid(u, v, indexing = 'ij')
-    tenth_percentile = ax1.contourf(U, V, low, cmap = 'viridis', alpha = alpha, norm=norm, levels=20)
-    ninetieth_percentile = ax2.contourf(U, V, high, cmap = 'viridis', alpha = alpha, norm=norm, levels=20)
-    ax1.grid(ls='--', alpha=alpha/2)
-    ax2.grid(ls='--', alpha=alpha/2)
+    tenth_percentile = plot_2dpdf(ax10, u, v, low, alpha, cmap, norm, levels)
+    ninetieth_percentile = plot_2dpdf(ax90, u, v, high, alpha, cmap, norm, levels)
     return tenth_percentile, ninetieth_percentile
 
 def plot_primary_pdfs(mpdfs, m1, names, label, result_dir, save=True):
@@ -49,14 +55,13 @@ def plot_primary_pdfs(mpdfs, m1, names, label, result_dir, save=True):
     for i in range(len(mpdfs)):
         plot_pdf(m1, mpdfs[i], names[i])
     plt.ylim(1e-5, 1e0)
-    plt.xlabel(r"$m_1$")
+    plt.xlabel(r"$m_1 [M_\odot]$")
     plt.legend()
     plt.xlim(m1[0], m1[-1])
     plt.show()
     if save:
         plt.savefig(result_dir + f"/mass_pdf_{label}.png", dpi=100)
     plt.close()
-
 
 def plot_mass_pdfs(mpdfs, qpdfs, m1, q, names, label, result_dir, save=True, colors=["red", "blue", "green"]):
 
@@ -84,7 +89,6 @@ def plot_mass_pdfs(mpdfs, qpdfs, m1, q, names, label, result_dir, save=True, col
     if save:
         plt.savefig(result_dir + f"/mass_ratio_pdf_{label}.png", dpi=100)
     plt.close()
-
 
 def plot_spin_pdfs(a_pdfs, tilt_pdfs, aa, cc, names, label, result_dir, save=True, colors=["red", "blue", "green"], secondary=False):
 
@@ -182,20 +186,74 @@ def plot_primary_chiq_pdfs(mpdfs, chiqpdfs, ms, chi_effs, qs, names, label, resu
         fig.savefig(result_dir + f"/chi-eff_mass-ratio_2d_pdf_{label}.png", dpi=100)
     plt.close()
 
-def plot_chiq_pdfs_stats(chiqpdfs, chi_effs, qs, label, result_dir, save=True):
+def plot_marginal_chi_q_pdfs(chieffpdfs, qpdfs, chi_effs, qs, chieff_lims, qmin, label, result_dir, save=True):
     fig = plt.figure(figsize=(16,9), layout='constrained')
-    cf_ax10 = fig.add_subplot(121)
-    cf_ax90 = fig.add_subplot(122)
-    axes_dict = {r'$10^\text{th}$ percentile': cf_ax10, r'$90^\text{th}$ percentile':cf_ax90}
+    for i in range(len(chieffpdfs)):
+        plot_pdf(chi_effs, chieffpdfs[i], label, loglog=False)
+    plt.xlabel(r'$\chi\textsubscript{eff}$')#_\text{eff}$')
+    plt.ylabel(r'$\tilde{p}(\chi\textsubscript{eff})$')
+    plt.title(r'$\tilde{p}(\chi\textsubscript{eff})=\int_{%s}^1dq\,p(\chi\textsubscript{eff},q)$' % qmin)
+    plt.show()
+    if save:
+        plt.savefig(result_dir + f"/marginal_effective_spin_pdf_{label}.png", dpi=100)
+    plt.close()
+
+    fig = plt.figure(figsize=(16,9), layout='constrained')
+    for i in range(len(qpdfs)):
+        plot_pdf(qs, qpdfs[i], label, loglog=False)
+    plt.xlabel(r'$q$')
+    plt.ylabel(r'$\tilde{p}(q)$')
+    plt.title(r'$\tilde{p}(q)=\int_{%s}^{%s}d\chi\textsubscript{eff}\,p(\chi\textsubscript{eff},q)$' % (chieff_lims[0],chieff_lims[-1]))
+    plt.show()
+    if save:
+        plt.savefig(result_dir + f"/marginal_mass-ratio_pdf_{label}.png", dpi=100)
+    plt.close()
+
+def plot_chiq_slices(chiqpdfs, chi_effs, qs, label, result_dir, save=True):
+    fig = plt.figure(figsize=(16,9), layout='constrained')
+    ax_chi_q1 = fig.add_subplot(121)
+    # ax_chi_q0 = fig.add_subplot(222)
+    # ax_q_chin1 = fig.add_subplot(223)
+    ax_q_chi0 = fig.add_subplot(122)
+    # axes_dict = {r'$p(\chi_\text{eff},q=1)$':ax_chi_q1, r'$p(\chi_\text{eff},q=0)$':ax_chi_q0, r'$p(\chi_\text{eff}=-1,q)$':ax_q_chin1, r'$p(\chi_\text{eff}=1,q)$':ax_q_chi1}
+    axes_dict = {r'$p(\chi\textsubscript{eff},q=1)$':ax_chi_q1, r'$p(\chi\textsubscript{eff}=0,q)$':ax_q_chi0}
     for i in range(len(chiqpdfs)):
-        cf10, cf90 = plot_2dstats(cf_ax10, cf_ax90, chi_effs, qs, chiqpdfs[i], label)
+        plot_pdf_axes(ax_chi_q1, chi_effs, chiqpdfs[i,:,:,-2], label=label, loglog=False)
+        plot_pdf_axes(ax_q_chi0, qs, chiqpdfs[i,:,len(chi_effs)//2,:], label=label, loglog=False)
+        # plot_pdf_axes(ax_chi_q0, chi_effs, chiqpdfs[i,:,:,0], label=label, loglog=False)
+        # plot_pdf_axes(ax_q_chin1, qs, chiqpdfs[i,:,0,:], label=label, loglog=False)
+        # plot_pdf_axes(ax_q_chi1, qs, chiqpdfs[i,:,-1,:], label=label, loglog=False)
+        # plot_pdf_axes(ax=, qs, chiqpdfs[i,:,], label=label, loglog=False)
     for key, value in axes_dict.items():
-        value.set_xlabel(r'$\chi_\text{eff}$')
+        if 'q=' in key:
+            value.set_xlabel(r'$\chi\textsubscript{eff}$')
+            value.set_ylabel(key)
+        else:
+            value.set_xlabel(r'$q$')
+            value.set_ylabel(key)
+    plt.show()
+    if save:
+        plt.savefig(result_dir + f"/chi_and_q_slices_pdf_{label}.png", dpi=100)
+    plt.close()
+
+def plot_chiq_pdfs_percentiles(chiqpdfs, chi_effs, qs, label, result_dir, save=True):
+    fig = plt.figure(figsize=(16,9), layout='constrained')
+    ax10_shared_cbar = fig.add_subplot(221)
+    ax90_shared_cbar = fig.add_subplot(222)
+    ax10 = fig.add_subplot(223)
+    ax90 = fig.add_subplot(224)
+    axes_dict = {r'$10\textsuperscript{th}$ percentile (shared colorbar)': ax10_shared_cbar, r'$90\textsuperscript{th}$ percentile (shared colorbar)':ax90_shared_cbar,
+                 r'$10\textsuperscript{th}$ percentile': ax10, r'$90\textsuperscript{th}$ percentile':ax90}
+    for i in range(len(chiqpdfs)):
+        cf10_shared_cbar, cf90_shared_cbar = plot_2dpercentiles(ax10_shared_cbar, ax90_shared_cbar, chi_effs, qs, chiqpdfs[i])
+        cf10, cf90 = plot_2dpercentiles(ax10, ax90, chi_effs, qs, chiqpdfs[i], shared_cbar=False)
+    cfs_dict = {r'$10\textsuperscript{th}$ percentile (shared colorbar)': cf10_shared_cbar, r'$90\textsuperscript{th}$ percentile (shared colorbar)':cf90_shared_cbar,
+                 r'$10\textsuperscript{th}$ percentile': cf10, r'$90\textsuperscript{th}$ percentile':cf90}
+    for key, value in axes_dict.items():
+        value.set_xlabel(r'$\chi\textsubscript{eff}$')
         value.set_ylabel(r'$q$')
         value.set_title(f'{key}')
-        # value.axis('scaled') # hmm
-    fig.colorbar(cf10, ax=cf_ax10)
-    fig.colorbar(cf90, ax=cf_ax90)
+        fig.colorbar(cfs_dict[key], ax=axes_dict[key])
     plt.show()
     if save:
         fig.savefig(result_dir + f"/chi-eff_mass-ratio_10th_90th_percentile_{label}.png", dpi=100)
@@ -204,37 +262,36 @@ def plot_chiq_pdfs_stats(chiqpdfs, chi_effs, qs, label, result_dir, save=True):
 def plot_chiq_pdfs_samples(chiqpdfs, chi_effs, qs, label, result_dir, save=True):
     fig = plt.figure(figsize=(16,9), layout='constrained')
     rng = np.random.default_rng()
-    rands = []
-    rands_idx = []
+    pdf = []
+    pdf_idx = []
     for i in range(10):
-        rng_idx = rng.integers(chiqpdfs[0].shape[0])
-        rands.append(chiqpdfs[0, rng_idx])
-        rands_idx.append(rng_idx)
-    rands = np.array(rands).reshape(2,5,*chiqpdfs[0,0].shape)
-    rands_idx = np.array(rands_idx).reshape(2,5)
-    norm = pltcolors.Normalize(rands.min(), rands.max())
+        rng_idx = rng.integers(chiqpdfs.shape[1])
+        pdf.append(chiqpdfs[0, rng_idx])
+        pdf_idx.append(rng_idx)
+    pdf = np.array(pdf).reshape(2,5,*chiqpdfs[0,0].shape)
+    pdf_idx = np.array(pdf_idx).reshape(2,5)
+    # norm = pltcolors.Normalize(pdf.min(), pdf.max())
     for i in range(2):
         for j in range(5):
             ax = plt.subplot2grid((2,5), (i,j))
-            cf = plot_2dpdf(ax, chi_effs, qs, rands[i,j], norm=norm)
-            ax.set_xlabel(r'$\chi_\text{eff}$')
+            cf = plot_2dpdf(ax, chi_effs, qs, pdf[i,j])#, norm=norm)
+            ax.set_xlabel(r'$\chi\textsubscript{eff}$')
             ax.set_ylabel(r'$q$')
-            ax.set_title(f'Sample {rands_idx[i,j]}')
-            # ax.axis('scaled') # hmm
+            ax.set_title(f'Sample {pdf_idx[i,j]} out of {chiqpdfs.shape[1]}')
             fig.colorbar(cf, ax=ax)
     plt.show()
     if save:
         fig.savefig(result_dir + f"/chi-eff_mass-ratio_random_samples_{label}.png", dpi=100)
     plt.close()
 
-def plot_chiq_pdfs_rng_stats(chiqpdfs, chi_effs, qs, label, result_dir, sample_frac = 1.0, save=True):
+def plot_chiq_pdfs_stats(chiqpdfs, chi_effs, qs, label, result_dir, sample_frac = 1.0, save=True):
     # add axes
     fig = plt.figure(figsize=(16,9), layout='constrained')
-    cf_mean_ax = fig.add_subplot(221, adjustable='box')
-    cf_med_ax = fig.add_subplot(222, adjustable='box')
-    cf_std_ax = fig.add_subplot(223, adjustable='box')
-    cf_frac_uncert_ax = fig.add_subplot(224, adjustable='box')
-    axes_dict = {'mean':cf_mean_ax, 'median':cf_med_ax, 'standard deviation':cf_std_ax, 'fractional uncertainty':cf_frac_uncert_ax}
+    mean_ax = fig.add_subplot(221, adjustable='box')
+    med_ax = fig.add_subplot(222, adjustable='box')
+    std_ax = fig.add_subplot(223, adjustable='box')
+    frac_uncert_ax = fig.add_subplot(224, adjustable='box')
+    axes_dict = {'mean':mean_ax, 'median':med_ax, 'standard deviation':std_ax, 'fractional uncertainty':frac_uncert_ax}
     # select random sample of pdfs
     rng = np.random.default_rng()
     print('Total number of random samples chosen', chiqpdfs.shape[1]*sample_frac)
@@ -252,15 +309,14 @@ def plot_chiq_pdfs_rng_stats(chiqpdfs, chi_effs, qs, label, result_dir, sample_f
         cmap = 'Reds' if key == 'fractional uncertainty' else 'viridis'
         norm = 'log' if key == 'fractional uncertainty' else 'linear'
         cf = plot_2dpdf(axes_dict[key], chi_effs, qs, value, cmap=cmap, norm=norm)
-        axes_dict[key].set_xlabel(r'$\chi_\text{eff}$')
+        axes_dict[key].set_xlabel(r'$\chi\textsubscript{eff}$')
         axes_dict[key].set_ylabel(r'$q$')
         axes_dict[key].set_title(f'{key}')
-        # axes_dict[key].axis('scaled') # hmm
         fig.colorbar(cf, ax=axes_dict[key])
 
     plt.show()
     if save:
-        fig.savefig(result_dir + f"/chi-eff_mass-ratio_random_samples_stats_{label}.png", dpi=100)
+        fig.savefig(result_dir + f"/chi-eff_mass-ratio_stats_{label}.png", dpi=100)
 
     plt.close()
 
